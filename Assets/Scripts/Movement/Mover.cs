64c238b2582f715db88d149c24a0ea6b3649015e
son.maiviet@gameloft.com <son.maiviet@gameloft.com>
2020-10-09 16:09:20 +0700

    Initialize PRGProject for learning unity3D and Implement combat basic

---
+++ Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace RPG.Movement
+{
+    public class Mover : MonoBehaviour
+    {
+        [SerializeField] Transform target;
+
+        Ray lastRay;
+
+        // Update is called once per frame
+        void Update()
+        {
+            UpdateAnimator();
+        }
+
+        private void UpdateAnimator()
+        {
+            Vector3 velecity = GetComponent<NavMeshAgent>().velocity;
+            Vector3 localVelocity = transform.InverseTransformDirection(velecity);
+            float speed = localVelocity.z;
+            GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+        }
+
+        private void MoveToCurrsor()
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            bool hasHit = Physics.Raycast(ray, out hit);
+            if (hasHit)
+            {
+                MoveTo(hit.point);
+            }
+        }
+
+        public void MoveTo(Vector3 destination)
+        {
+            GetComponent<NavMeshAgent>().destination = destination;
+        }
+    }
+}
