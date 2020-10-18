a2bf933c26fbd0f52496ab9c58f92abc04aa564e
Sonmv <maivietson.nd@gmail.com>
2020-10-18 17:25:12 +0700

    *[son.mv][Unity3D]: Implement animation for attack and Throttling Our Attacks

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -9,11 +9,15 @@ namespace RPG.Combat
     public class Fighter : MonoBehaviour, IAction
     {
         [SerializeField] float weaponRange = 2.0f;
+        [SerializeField] float timeBetweenAttack = 1f;
 
         Transform target;
+        float timeSinceLastAttack = 0f;
 
         private void Update()
         {
+            timeSinceLastAttack += Time.deltaTime;
+
             if (target == null) return;
 
             if (!GetIsInRange())
@@ -23,6 +27,16 @@ namespace RPG.Combat
             else
             {
                 GetComponent<Mover>().Cancel();
+                AttackBehaviour();
+            }
+        }
+
+        private void AttackBehaviour()
+        {
+            if (timeSinceLastAttack > timeBetweenAttack)
+            {
+                GetComponent<Animator>().SetTrigger("attack");
+                timeSinceLastAttack = 0f;
             }
         }
 
@@ -40,6 +54,12 @@ namespace RPG.Combat
         public void Cancel()
         {
             target = null;
+        }
+
+        // Animation event
+        void Hit()
+        {
+
         }
     }
 }
